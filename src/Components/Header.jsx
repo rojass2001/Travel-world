0e00b9f4f9164227fa77df4ff92d679be2e2ca05
rojass2001 <rojascherian@gmail.com>
2025-04-04 23:54:19 +0530

    third commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import { FaBars } from 'react-icons/fa'
 import Mobileheader from './Mobileheader'
+import { motion } from 'framer-motion'
  const headerdiv="fixed flex px-3 justify-between  w-[98%] h-[55px] bg-white top-[20px] top-0 left-0 right-0 mx-auto items-center z-50  "
 const navlink=" hover:bg-gray-400    "
  function Header() {
@@ -11,32 +12,31 @@ const navlink=" hover:bg-gray-400    "
   setopen(!open)
  }
   return (
-    <footer>
-      <div className={headerdiv}>
-      <h2 className="text-2xl font-bold">Travel</h2>
-      <div className='flex items-center '>
-      <div className="flex  text-xl font-bold cursor-pointer space-x-7  sm:hidden">
-      <Link to='/'>
-      <p className={navlink}>Home</p>
-      </Link>
-      <Link to='/gallery'>
-      <p className={navlink}>Gallery</p>
-      </Link>
-      <Link to='/about'>
-      <p className={navlink}>about</p>
-      </Link>
-      <Link to='/contact'>
-      <p className={navlink}>Contact</p>
-      </Link>
-     
-      </div>
-      <button onClick={navbaropen}> <FaBars className='text-black text-2xl font-bold hidden sm:block  '/></button>
-      </div>
-       
-          <Mobileheader navbaropen={navbaropen} open={open} />
+   
+    <motion.header className={headerdiv}>
+        <h2 className="text-2xl font-bold">Travel</h2>
+        <div className='flex items-center '>
+        <div className="flex  text-xl font-bold cursor-pointer space-x-7  sm:hidden">
+        <Link to='/'>
+        <p className={navlink}>Home</p>
+        </Link>
+        <Link to='/gallery'>
+        <p className={navlink}>Gallery</p>
+        </Link>
+        <Link to='/about'>
+        <p className={navlink}>about</p>
+        </Link>
+        <Link to='/contact'>
+        <p className={navlink}>Contact</p>
+        </Link>
+      
+        </div>
+        <button onClick={navbaropen}> <FaBars className='text-black text-2xl font-bold hidden sm:block  '/></button>
+        </div>
         
-      </div>
-    </footer>
+            <Mobileheader navbaropen={navbaropen} open={open} />
+          
+    </motion.header>
   )
 }
 
--- src/Components/Mobileheader.jsx
+++ src/Components/Mobileheader.jsx
@@ -6,16 +6,15 @@ import React from 'react'
 
 function Mobileheader({navbaropen,open }) {
     return (
-        <AnimatePresence mode="wait">
-            {open && (
-                <motion.div
-             
-                    initial={{ opacity: 0, y: -100 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: -100 }}
-                    transition={{ duration: 0.3 }}
-                    className=' hidden rounded-xl shadow-md shadow-gray-300 w-[calc(100%-40px)] h-[400px] bg-gray-200  absolute top-[60px] 
-       mr-[20px] ml-[20px] text-xl font-bold cursor-pointer space-x-7 sm:block md:block items-center content-center  '>
+    <AnimatePresence mode="wait">
+        {open && (
+        <motion.header
+             initial={{ opacity: 0, y: -100 }}
+             animate={{ opacity: 1, y: 0 }}
+             exit={{ opacity: 0, y: -100 }}
+             transition={{ duration: 0.3 }}
+             className=' hidden rounded-xl shadow-md shadow-gray-300 w-[calc(100%-40px)] h-[400px] bg-gray-200  absolute top-[60px] 
+                        mr-[20px] ml-[20px] text-xl font-bold cursor-pointer space-x-7 sm:block md:block items-center content-center  '>
                     <Link to='/'>
                         <p onClick={navbaropen} className=' px-2  '>Home</p>
                     </Link>
@@ -28,9 +27,9 @@ function Mobileheader({navbaropen,open }) {
                     <Link to='/contact'>
                         <p onClick={navbaropen} className=' px-2  '>Contact</p>
                     </Link>
-                </motion.div>
+        </motion.header>
             )}
-            </AnimatePresence>
+   </AnimatePresence>
   )
 }
 
